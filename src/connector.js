f3ce68993f3ef7a5a7e1a90a361dce965d0a66ca
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-03 02:16:15 +0530

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -1,17 +1,20 @@
+const mongoURI = "mongodb://localhost:27017" + "/collegeDetails";
 
-//const mongodb = require('mongodb');
+let mongoose = require("mongoose");
+const { collegeSchema } = require("./schema");
 
-const mongoURI = "mongodb://localhost:27017" + "/collegeDetails"
-
-let mongoose = require('mongoose');
-const { collegeSchema } = require('./schema')
-
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-let collegeModel = mongoose.model('collegerecords', collegeSchema)
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connection established with mongodb server online");
+  })
+  .catch((err) => {
+    console.log("error while connection", err);
+  });
+let collegeModel = mongoose.model("collegerecords", collegeSchema);
+let data = require("./data");
+collegeModel.deleteMany().then((e) => {
+  collegeModel.insertMany(data.data);
+});
 
 exports.connection = collegeModel;
